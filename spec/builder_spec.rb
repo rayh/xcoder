9bbeeb31ad021ca94707085f0d58c7f4ccd4d0ff
Franklin Webber <franklin.webber@gmail.com>
2012-02-07 01:25:03 -0800

    Re-added integration tests with builder; removed override tests

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -10,7 +10,22 @@ describe Xcode::Builder do
     let(:subject) { configuration.builder }
 
     describe "#build" do
-
+      
+      it "should be able to build" do
+        subject.clean
+        subject.build
+        File.exists?(subject.app_path).should==true
+        File.exists?(subject.dsym_path).should==true
+      end
+      
+      it "should be able to package" do
+        subject.clean
+        subject.build
+        subject.package
+        File.exists?(subject.dsym_zip_path).should==true
+        File.exists?(subject.ipa_path).should==true
+      end
+      
       let(:default_build_parameters) do
         [ "xcodebuild", 
           "-sdk #{configuration.target.project.sdk}", 
@@ -24,54 +39,6 @@ describe Xcode::Builder do
       it "should build the project with the default parameters" do
         Xcode::Shell.should_receive(:execute).with(default_build_parameters)
         subject.build
-      end
-
-      context "when additional parameters have been specified" do
-
-        it "should add new parameters" do
-
-          expected_build_parameters = default_build_parameters + [ "-additionalparameter=value" ]
-
-          Xcode::Shell.should_receive(:execute).with(expected_build_parameters)
-
-          subject.build do |parameters|
-            parameters << "-additionalparameter=value"
-          end
-
-        end
-
-        let(:overridden_build_parameters) do
-          [ "xcodebuild", 
-            "-sdk newiossdk", 
-            "-project \"#{configuration.target.project.path}\"", 
-            "-target \"#{configuration.target.name}\"", 
-            "-configuration \"#{configuration.name}\"", 
-            "OBJROOT=\"#{File.dirname(configuration.target.project.path)}/build/\"", 
-            "SYMROOT=\"#{File.dirname(configuration.target.project.path)}/build/\"" ]
-        end
-
-        it "should override existing parameters" do
-
-          Xcode::Shell.should_receive(:execute).with(overridden_build_parameters)
-
-          subject.build do |parameters|
-            parameters << "-sdk newiossdk"
-          end
-
-        end
-
-        it "should support additional environment variables" do
-
-          expected_build_parameters = default_build_parameters + [ "ENVIRONMENTVARIABLE=NEWVARIABLE" ]
-
-          Xcode::Shell.should_receive(:execute).with(expected_build_parameters)
-
-          subject.build do |parameters|
-            parameters << "ENVIRONMENTVARIABLE=NEWVARIABLE"
-          end
-
-        end
-
       end
 
     end
@@ -105,6 +72,21 @@ describe Xcode::Builder do
     let(:subject) { scheme.builder }
 
     describe "#build" do
+      
+      it "should be able to build" do
+        subject.clean
+        subject.build
+        File.exists?(subject.app_path).should==true
+        File.exists?(subject.dsym_path).should==true
+      end
+      
+      it "should be able to package" do
+        subject.clean
+        subject.build
+        subject.package
+        File.exists?(subject.dsym_zip_path).should==true
+        File.exists?(subject.ipa_path).should==true
+      end
 
       let(:default_build_parameters) do
         [ "xcodebuild",
@@ -118,53 +100,6 @@ describe Xcode::Builder do
       it "should build the project with the default parameters" do
         Xcode::Shell.should_receive(:execute).with(default_build_parameters)
         subject.build
-      end
-
-      context "when additional parameters have been specified" do
-
-        it "should add new parameters" do
-
-          expected_build_parameters = default_build_parameters + [ "-additionalparameter=value" ]
-
-          Xcode::Shell.should_receive(:execute).with(expected_build_parameters)
-
-          subject.build do |parameters|
-            parameters << "-additionalparameter=value"
-          end
-
-        end
-
-        let(:overridden_build_parameters) do
-          [ "xcodebuild",
-            "-sdk newiossdk",
-            "-project \"#{scheme.project.path}\"",
-            "-scheme #{scheme.name}",
-            "OBJROOT=\"#{File.dirname(scheme.project.path)}/build/\"", 
-            "SYMROOT=\"#{File.dirname(scheme.project.path)}/build/\"" ]
-        end
-
-        it "should override existing parameters" do
-
-          Xcode::Shell.should_receive(:execute).with(overridden_build_parameters)
-
-          subject.build do |parameters|
-            parameters << "-sdk newiossdk"
-          end
-
-        end
-
-        it "should support additional environment variables" do
-
-          expected_build_parameters = default_build_parameters + [ "ENVIRONMENTVARIABLE=NEWVARIABLE" ]
-
-          Xcode::Shell.should_receive(:execute).with(expected_build_parameters)
-
-          subject.build do |parameters|
-            parameters << "ENVIRONMENTVARIABLE=NEWVARIABLE"
-          end
-
-        end
-
       end
       
     end
@@ -187,23 +122,6 @@ describe Xcode::Builder do
       end
 
     end
-    
-    # it "should be able to build" do
-    #   builder = @scheme.builder
-    #   builder.clean
-    #   builder.build
-    #   File.exists?(builder.app_path).should==true
-    #   File.exists?(builder.dsym_path).should==true
-    # end
-    # 
-    # it "should be able to package" do
-    #   builder = @scheme.builder
-    #   builder.clean
-    #   builder.build
-    #   builder.package
-    #   File.exists?(builder.dsym_zip_path).should==true
-    #   File.exists?(builder.ipa_path).should==true
-    # end
 
   end
   
