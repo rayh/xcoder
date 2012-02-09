10a531b1207515f1f46595682e9433ee16a5a035
Franklin Webber <franklin.webber@gmail.com>
2012-02-08 22:58:14 -0800

    Creation of an entire target with Cedar example

---
+++ spec/TestProject/Specs/main2Spec.m
@@ -0,0 +1,22 @@
+//
+//  main2Spec.m
+//  TestProject
+//
+//  Created by Franklin Webber on 2/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "main2.h"
+
+@interface main2 (Specs)
+
+@end
+
+SPEC_BEGIN(main2Spec)
+
+describe(@"main2", ^(void) {
+
+
+});
+
+SPEC_END
