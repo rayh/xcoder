c51f5b09fdffbf8f99b26a2aae0088dbd132c2b9
Franklin Webber <franklin.webber@gmail.com>
2012-02-09 17:17:07 -0800

    Updated the pull-to-refresh example to be mindful of existing files

--- spec/integration/pull_to_refresh_install_spec.rb
+++ spec/integration/pull_to_refresh_install_spec.rb
@@ -16,11 +16,15 @@ describe "EGORefreshTableHeaderView", :integration => true do
     ptr_group = project.group('Vendor/EGORefreshTableHeaderView')
     
     # Add the source and header file to the project
-    # @todo this currently will add the files multiple times based on the application 
-    #   execution. This should be addressed to check before adding.
     
-    ptr_source = ptr_group.create_file 'name' => 'EGORefreshTableHeaderView.m', 'path' => 'Vendor/EGORefreshTableHeaderView/EGORefreshTableHeaderView.m'
-    ptr_header = ptr_group.create_file 'name' => 'EGORefreshTableHeaderView.h', 'path' => 'Vendor/EGORefreshTableHeaderView/EGORefreshTableHeaderView.h'
+    source_files = [ { 'name' => 'EGORefreshTableHeaderView.m', 'path' => 'Vendor/EGORefreshTableHeaderView/EGORefreshTableHeaderView.m' },
+                     { 'name' => 'EGORefreshTableHeaderView.h', 'path' => 'Vendor/EGORefreshTableHeaderView/EGORefreshTableHeaderView.h' } ]
+    
+    source_files.each do |source|
+      ptr_group.create_file(source) unless ptr_group.file(source['name'])
+    end
+    
+    ptr_source = ptr_group.file('EGORefreshTableHeaderView.m')
     
     # Select the main target of the project and add the source file to the build
     # phase.
@@ -28,8 +32,9 @@ describe "EGORefreshTableHeaderView", :integration => true do
     # @todo this currently will add the files multiple times based on the application 
     #   execution. This should be addressed to check before adding.
     
-    install_target = project.target('TestProject')
-    install_target.sources_build_phase.add_build_file ptr_source
+    source_build_phase = project.target('TestProject').sources_build_phase
+    
+    source_build_phase.add_build_file(ptr_source) unless source_build_phase.build_file(ptr_source.name)
     
     project.save!
   end
