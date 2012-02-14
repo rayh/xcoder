0178f0515a5e8550d4c47aa548f3de88cb884ab6
Franklin Webber <franklin.webber@gmail.com>
2012-02-14 01:40:34 -0800

    Cleaned up the Cedar Install Example

--- spec/integration/cedar_install_spec.rb
+++ spec/integration/cedar_install_spec.rb
@@ -21,70 +21,45 @@ describe "Cedar", :integration => true do
       target.name = 'Specs'
       target.product_name = 'Specs'
       
-      # @todo create the following files within the project file
-    
-      # E21EB9D614E357CF0058122A /* Specs.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = Specs.app; sourceTree = BUILT_PRODUCTS_DIR; };
-      # Adding an application needs to set the productReference in the target
-      # productReference = E21EB9D614E357CF0058122A /* Specs.app */;
-    
       application_file = target.create_product_reference 'Specs'
       
       # Create the Specs group
     
-      specs_group = project.groups.create_group('Specs')
-      supporting_group = specs_group.create_group('Supporting Files')
+      supporting_group = project.group('Specs/SupportingFiles')
+
+      # Create the Source File and add it to the sources build phase
     
-    
-      # E21EB9E114E357CF0058122A /* main.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = main.m; sourceTree = "<group>"; };
       main_source = supporting_group.create_file 'name' => 'main.m', 'path' => 'Specs/main.m'
     
       target.create_build_phase :sources do |source|
         source.add_build_file main_source
       end
-    
-      # E21EB9DD14E357CF0058122A /* Specs-Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = "Specs-Info.plist"; sourceTree = "<group>"; };
-    
+      
+      # Create the InfoPlist file and add it to the Resrouces build phase
+      
       supporting_group.create_file 'name' => 'Specs-Info.plist', 'path' => 'Specs/Specs-Info.plist'
-    
-      # E2B8B9F214E395B400D08897 /* InfoPlist.strings */ = {
-      #   isa = PBXVariantGroup;
-      #   children = (
-      #     E2B8B9F314E395B400D08897 /* en */,
-      #   );
-      #   name = InfoPlist.strings;
-      #   path = Specs/en.lproj;
-      #   sourceTree = "<group>";
-      # };
-          
+        
       infoplist_file = supporting_group.create_infoplist 'name' => 'InfoPlist.strings', 'path' => 'Specs'
       infoplist_file.create_file 'name' => 'en', 'path' => 'en.lproj/InfoPlist.strings'
       
-      # E21EB9DF14E357CF0058122A /* en */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = en; path = en.lproj/InfoPlist.strings; sourceTree = "<group>"; };
-      # infofile = supporting_group.create_file 'name' => 'en', 'path' => 'en.lproj/InfoPlist.strings'
-    
       target.create_build_phase :resources do |resources|
         resources.add_build_file infoplist_file
       end
     
-      # E21EB9E314E357CF0058122A /* Specs-Prefix.pch */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = "Specs-Prefix.pch"; sourceTree = "<group>"; };
       prefix_file = supporting_group.create_file 'name' => 'Specs-Prefix.pch', 'path' => 'Specs/Specs-Prefix.pch'
-    
-      # E21EB9D814E357CF0058122A /* UIKit.framework in Frameworks */,
-      # 7165D454146B4EA100DE2F0E /* UIKit.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = UIKit.framework; path = System/Library/Frameworks/UIKit.framework; sourceTree = SDKROOT; };
-      # E21EB9D914E357CF0058122A /* Foundation.framework in Frameworks */,
-      # E21EB9DA14E357CF0058122A /* CoreGraphics.framework in Frameworks */,
-    
-      # E21EB9EE14E359840058122A /* Cedar-iPhone.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = "Cedar-iPhone.framework"; path = "Vendor/Frameworks/Cedar-iPhone.framework"; sourceTree = "<group>"; };
-      cedar_framework = target.project.frameworks_group.create_framework 'name' => 'Cedar-iPhone.framework', 'path' => 'Vendor/Frameworks/Cedar-iPhone.framework', 'sourceTree' => '<group>'
       
-      # @todo this is dumb to have the first being called. There must be a more clear way
+      # Create the custom framework and add it to the frameworks build phase
+      
+      cedar_framework = target.project.frameworks_group.create_framework 'name' => 'Cedar-iPhone.framework', 'path' => 'Vendor/Frameworks/Cedar-iPhone.framework', 'sourceTree' => '<group>'
       
       target.create_build_phase :framework do |frameworks|
         frameworks.add_build_file cedar_framework
-        frameworks.add_build_file target.project.frameworks_group.file('UIKit.framework').first
-        frameworks.add_build_file target.project.frameworks_group.file('Foundation.framework').first
-        frameworks.add_build_file target.project.frameworks_group.file('CoreGraphics.framework').first
+        frameworks.add_build_file project.file('Frameworks/UIKit.framework')
+        frameworks.add_build_file project.file('Frameworks/Foundation.framework')
+        frameworks.add_build_file project.file('Frameworks/CoreGraphics.framework')
       end
+      
+      # Add the necessary configurations
       
       target.create_configurations :debug, :release do |config|
         config.set 'GCC_PREFIX_HEADER', 'Specs/Specs-Prefix.pch'
