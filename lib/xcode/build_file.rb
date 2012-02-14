bc4dc53f6e2a7e910d0dcf48ecf77d02dc952065
Franklin Webber <franklin.webber@gmail.com>
2012-02-13 23:35:43 -0800

    Add a build file without ARC

--- lib/xcode/build_file.rb
+++ lib/xcode/build_file.rb
@@ -14,8 +14,26 @@ module Xcode
     # @param [String] file_identifier the unique identifier for the file
     # @return [Hash] the properties hash for a default BuildFile.
     # 
-    def self.with_properties file_identifier
+    def self.buildfile file_identifier
       { 'isa' => "PBXBuildFile", 'fileRef' => file_identifier }
+    end
+
+    #
+    # Create the properties hash for a build file with the given file reference
+    # identifier. This build file will not be compiled under ARC.
+    # 
+    # @example build file without ARC 
+    # 
+    #     {isa = PBXBuildFile; fileRef = 98A45A5E14BDF4580079B105 /* Reachability.m */; settings = {COMPILER_FLAGS = "-fno-objc-arc"; }; };
+    # 
+    # @param [String] file_identifier the unique identifier for the file
+    # @return [Hash] the properties hash for a BuildFile that will not be
+    #   compiled under ARC.
+    #
+    def self.buildfile_without_arc file_identifier
+      { 'isa' => "PBXBuildFile", 
+        'fileRef' => file_identifier, 
+        'settings' => { 'COMPILER_FLAGS' => "-fno-objc-arc" } }
     end
     
   end
--- lib/xcode/build_phase.rb
+++ lib/xcode/build_phase.rb
@@ -76,13 +76,53 @@ module Xcode
     # entry is added to the particular build phase. A BuildFile identifier must
     # exist for each target.
     # 
+    # @example adding a source file to the sources build phase
+    # 
+    #     spec_file = project.group('Specs/Controller').create_file('FirstControllerSpec.m')
+    #     project.target('Specs').sources_build_phase.add_build_file spec_file
+    # 
+    # @example adding a source file, that does not use ARC, to the sources build phase
+    # 
+    #     spec_file = project.group('Specs/Controller').create_file('FirstControllerSpec.m')
+    #     project.target('Specs').sources_build_phase.add_build_file spec_file, { 'COMPILER_FLAGS' => "-fno-objc-arc" }
+    # 
     # @param [FileReference] file the FileReference Resource to add to the build 
     # phase.
+    # @param [Hash] settings additional build settings that are specifically applied
+    #   to this individual file.
     #
     def add_build_file(file)
+      add_build_file_with_file(file) { @registry.add_object BuildFile.buildfile(file.identifier) }
+    end
+    
+    #
+    # Add the specified file to the Build Phase.
+    # 
+    # First a BuildFile entry is created for the file and then the build file
+    # entry is added to the particular build phase. A BuildFile identifier must
+    # exist for each target.
+    # 
+    # @example adding a source file, that does not use ARC, to the sources build phase
+    # 
+    #     spec_file = project.group('Specs/Controller').create_file('FirstControllerSpec.m')
+    #     project.target('Specs').sources_build_phase.add_build_file spec_file, { 'COMPILER_FLAGS' => "-fno-objc-arc" }
+    # 
+    # @param [FileReference] file the FileReference Resource to add to the build 
+    # phase.
+    # @param [Hash] settings additional build settings that are specifically applied
+    #   to this individual file.
+    #
+    def add_build_file_without_arc(file)
+      add_build_file_with_file(file) { @registry.add_object BuildFile.buildfile_without_arc(file.identifier) }
+    end
+    
+    private
+    
+    
+    def add_build_file_with_file(file,&block)
       find_file_by = file.name || file.path
       unless build_file(find_file_by)
-        new_build_file = @registry.add_object BuildFile.with_properties(file.identifier)
+        new_build_file = block.call
         @properties['files'] << new_build_file.identifier
       end
     end
