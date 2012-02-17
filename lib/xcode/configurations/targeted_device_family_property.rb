51277704a14cc28767ff93c2b24402adeebf9678
Franklin Webber <franklin.webber@gmail.com>
2012-02-16 19:20:05 -0800

    Fixes for Boolean Property and Target Device Family Property

--- lib/xcode/configurations/targeted_device_family_property.rb
+++ lib/xcode/configurations/targeted_device_family_property.rb
@@ -17,7 +17,7 @@ module Xcode
       # @return [Array<String>] the platform names supported.
       #
       def open(value)
-        value.split(",").map do |platform_number|
+        value.to_s.split(",").map do |platform_number|
           platforms[platform_number]
         end
       end
@@ -29,14 +29,14 @@ module Xcode
       #
       def save(value)
         Array(value).map do |platform_name|
-          platforms.map {|number,name| number if name == platform_name }
-        end.flatten.join(",")
+          platforms.map {|number,name| number if name.to_s == platform_name.to_s.downcase }
+        end.flatten.compact.join(",")
       end
   
       private
   
       def platforms
-        { 1 => 'iPhone', 2 => 'iPad' }
+        { "1" => :iphone, "2" => :ipad }
       end
   
     end
