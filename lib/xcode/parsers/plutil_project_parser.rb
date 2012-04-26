d8e4c56778867561ffb7958f703fad95c11d2b34
Franklin Webber <franklin.webber@gmail.com>
2012-04-25 20:06:51 -0700

    Raising an error message when the file is not parsed correctly

--- lib/xcode/parsers/plutil_project_parser.rb
+++ lib/xcode/parsers/plutil_project_parser.rb
@@ -10,29 +10,45 @@ module Xcode
     # Using the sytem tool plutil, the specified project file is parsed and 
     # converted to XML, and then converted into a ruby hash object.
     # 
-    def parse path
-      xml = `plutil -convert xml1 -o - "#{path}"`
-      Plist::parse_xml(xml)
+    def parse(path)
+      registry = Plist.parse_xml open_project_file(path)
+      
+      raise "Failed to correctly parse the project file #{path}" unless registry
+      
+      registry
     end
 
     #
     # Save the outputed string data to the specified project file path and then
     # formats that data to be prettier than the default output.
     # 
-    def save path,data
+    def save(path,data)
      
       File.open(path,'w') do |file|
         file.puts data
       end
 
-      #
-      
       if File.exists?(path)
         `pl -input #{path} -output #{path}`
       end
 
-   end
-  
+    end
+    
+    private
+    
+    #
+    # @return [String] an XML version of the project file or the error message
+    #   that the file could not be found.
+    # 
+    # @example Error Message
+    # 
+    #       Project.xcodeproj/project.pbproj file does not exist or is not 
+    #       readable or is not a regular file.
+    # 
+    def open_project_file(path)
+      `plutil -convert xml1 -o - "#{path}"`
+    end
+      
   end
   
 end
