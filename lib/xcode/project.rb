21791518b3b59cf95b8eb4cddbbc449146e59675
Franklin Webber <franklin.webber@gmail.com>
2012-11-13 13:05:35 -0500

    Scheme - scheme creation with a root path
    The root path was being passed in and we were finding the shared and user
    specific schemes. From within the scheme we were having to backtrack a
    large number of directories to find the root again (plus one more out)
    to find the project.
    
    Instead we are just passing in the root when we create the scheme.
    
    Cleaned up some of the methods to generate the schemes and scheme paths

--- lib/xcode/project.rb
+++ lib/xcode/project.rb
@@ -3,145 +3,145 @@ require 'xcode/resource'
 require 'xcode/registry'
 
 module Xcode
-  
+
   #
   # The project is the representation of an Xcode Project folder, the `*.xcodeproj`,
   # folder that contains a number of workspace files and project files like
-  # `project.pbxproj`. 
-  # 
-  # 
+  # `project.pbxproj`.
+  #
+  #
   # The Project represents encapsulates the an actual Resource that is ProjectReference
   # and other objects.
-  # 
-  class Project 
-    
-    # @return [String] the name of the project; This value is deteremined from the 
-    #   first part of the Xcode project folder name (e.g. "TestProject.xcodeproj" 
+  #
+  class Project
+
+    # @return [String] the name of the project; This value is deteremined from the
+    #   first part of the Xcode project folder name (e.g. "TestProject.xcodeproj"
     #   name is "TestProject")
     attr_reader :name
-    
+
     # @return [String] the sdks for the project. This is specified during the project
     #   initialization. If none are provided this currently defaults to "iphoneos"
     attr_reader :sdk
-    
+
     # @return [String] the expanded file path for the project. This is expanded from the
     #   file path specified during initialization.
     attr_reader :path
-    
+
     # @return [Array<Scheme>] the schemes that are found within the project path.
     attr_reader :schemes
-    
+
     # @return [Registry] the data that is parsed from the project.pbxproj, it is
     #   in most part a Hash with additional methods included to provide core
     #   functionality.
     attr_reader :registry
-    
-    # @return [ProjectReference] the project object that is contained in the 
+
+    # @return [ProjectReference] the project object that is contained in the
     #   project file that contains additional information
     attr_reader :project
-    
+
     #
     # Initialized with a specific path and sdk.
-    # 
+    #
     # This initialization is not often used. Instead projects are generated
     # through the Xcode#project method.
-    # 
+    #
     # @see Xcode
     #
     # @param [String] path of the project to open.
-    # @param [String] sdk the sdk value of the project. This will default to 
+    # @param [String] sdk the sdk value of the project. This will default to
     #   `iphoneos`.
-    # 
+    #
     def initialize(path, sdk=nil)
       @sdk = sdk || "iphoneos"  # FIXME: should support OSX/simulator too
       @path = File.expand_path path
       @schemes = nil
       @groups = []
       @name = File.basename(@path).gsub(/\.xcodeproj/,'')
-      
+
       # Parse the Xcode project file and create the registry
-      
+
       @registry = parse_pbxproj
       raise "Unable to parse project at #{@path}" if @registry.nil?
-      @project = Xcode::Resource.new @registry.root, @registry      
+      @project = Xcode::Resource.new @registry.root, @registry
     end
-    
+
     #
     # @return [Array<Xcode::Scheme>] available schemes for the project
     #
     def schemes
       return @schemes unless @schemes.nil?
-      @schemes = Xcode::Scheme.find_in_path @path
+      @schemes = Xcode::Scheme.find_in_project self
     end
-    
-    
+
+
     #
     # @return [Fixnum] the project's object version
-    # 
+    #
     def object_version
       @registry.object_version
     end
 
     #
     # @return [Fixnum] the project's archive version
-    # 
+    #
     def archive_version
       @registry.archive_version
     end
-    
+
     #
     # @return [Array<Configuration>] a list of configurations global to the project
-    # 
+    #
     def global_configs
       @project.configs
     end
-    
+
     #
     # @param [String,Symbol] name of the configuration for the project
-    # 
+    #
     # @return [Configuration] the project level configuration with the given name;
     #   raise an exception if no configuration exists with that name.
     #
     def global_config(name)
       @project.config(name)
     end
-    
+
     #
     # Returns the main group of the project where all the files reside.
-    # 
+    #
     # @todo this really could use a better name then groups as it is the main group
     #   but it should likely be something like main_group, root or something
     #   else that conveys that this is the project root for files, and such.
-    # 
+    #
     # @return [Group] the main group, the heart of the action of the file
     #   explorer for the Xcode project. From here all other groups and items
     #   may be found.
-    # 
+    #
     def groups
       @project.main_group
     end
-    
+
     #
     # Returns the group specified. If any part of the group does not exist along
-    # the path the group is created. Also paths can be specified to make the 
+    # the path the group is created. Also paths can be specified to make the
     # traversing of the groups easier.
-    # 
+    #
     # @note this will attempt to find the paths specified, if it fails to find them
     #   it will create one and then continue traversing.
     #
     # @example Traverse a path through the various sub-groups.
-    # 
+    #
     #     project.group('Vendor/MyCode/Support Files')
     #     # is equivalent to ...
     #     project.group('Vendor').first.group('MyCode').first.group('Supporting Files')
-    # 
+    #
     # @note this path functionality current is only exercised from the project level
     #   all groups will treat the path division `/` as simply a character.
-    # 
+    #
     # @param [String] name the group name to find/create
-    # 
+    #
     # @return [Group] the group with the specified name.
-    # 
+    #
     def group(name,options = {},&block)
       @project.group(name,options,&block)
     end
@@ -157,81 +157,81 @@ module Xcode
       path, name = File.split(name_with_path)
       group(path).file(name).first
     end
-    
+
     #
-    # Most Xcode projects have a products group where products are placed. This 
+    # Most Xcode projects have a products group where products are placed. This
     # will generate an exception if there is no products group.
-    # 
+    #
     # @return [Group] the 'Products' group of the project.
     def products_group
       current_group = groups.group('Products').first
       current_group.instance_eval(&block) if block_given? and current_group
       current_group
     end
-    
+
     #
-    # Most Xcode projects have a Frameworks gorup where all the imported 
+    # Most Xcode projects have a Frameworks gorup where all the imported
     # frameworks are shown. This will generate an exception if there is no
     # Frameworks group.
-    # 
+    #
     # @return [Group] the 'Frameworks' group of the projet.
     def frameworks_group
       current_group = groups.group('Frameworks').first
       current_group.instance_eval(&block) if block_given? and current_group
       current_group
     end
-    
+
     #
-    # This will convert the current project file into a supported Xcode Plist 
+    # This will convert the current project file into a supported Xcode Plist
     # format. This format is not json or a traditional plist so several core
     # Ruby objects gained the #to_xcplist method to save it properly.
-    # 
-    # Specifically this will add the necessary file header information and the 
+    #
+    # Specifically this will add the necessary file header information and the
     # surrounding mustache braces around the xcode plist format of the registry.
-    # 
+    #
     # @return [String] Xcode Plist format of the project.
     def to_xcplist
-      
+
       # @note The Hash#to_xcplist, which the Registry will save out as xcode,
-      #   saves a semi-colon at the end which needs to be removed to ensure 
+      #   saves a semi-colon at the end which needs to be removed to ensure
       #   the project file can be opened.
-      
+
       %{// !$*UTF8*$!"\n#{@registry.to_xcplist.gsub(/\};\s*\z/,'}')}}
     end
-    
-    # 
+
+    #
     # Save the current project at the current path that it exists.
-    # 
+    #
     def save!
       save @path
     end
-    
+
     #
     # Saves the current project at the specified path.
-    # 
-    # @note currently this does not support saving the workspaces associated 
+    #
+    # @note currently this does not support saving the workspaces associated
     #   with the project to their new location.
-    # 
+    #
     # @param [String] path the path to save the project
     #
     def save(path)
-      
+
       Dir.mkdir(path) unless File.exists?(path)
       project_filepath = "#{path}/project.pbxproj"
-      
+
       # @toodo Save the workspace when the project is saved
       # FileUtils.cp_r "#{path}/project.xcworkspace", "#{path}/project.xcworkspace"
-  
+
       Xcode::PLUTILProjectParser.save "#{path}/project.pbxproj", to_xcplist
-      
+
     end
-    
+
     #
-    # Return the scheme with the specified name. Raises an error if no schemes 
+    # Return the scheme with the specified name. Raises an error if no schemes
     # match the specified name.
-    # 
+    #
     # @note if two schemes match names, the first matching scheme is returned.
-    # 
+    #
     # @param [String] name of the specific scheme
     # @return [Scheme] the specific scheme that matches the name specified
     #
@@ -241,26 +241,26 @@ module Xcode
       yield scheme if block_given?
       scheme
     end
-    
+
     #
     # All the targets specified within the project.
-    # 
+    #
     # @return [Array<Target>] an array of all the available targets for
     #   the specific project.
-    # 
+    #
     def targets
       @project.targets.map do |target|
         target.project = self
         target
       end
     end
-    
+
     #
     # Return the target with the specified name. Raises an error if no targets
     # match the specified name.
-    # 
+    #
     # @note if two targets match names, the first matching target is returned.
-    # 
+    #
     # @param [String] name of the specific target
     # @return [Target] the specific target that matches the name specified
     #
@@ -270,55 +270,55 @@ module Xcode
       yield target if block_given?
       target
     end
-    
+
     #
     # Creates a new target within the Xcode project. This will by default not
     # generate all the additional build phases, configurations, and files
     # that create a project.
-    # 
+    #
     # Available targts:
-    # 
+    #
     # * native
     # * aggregate
-    # 
+    #
     # @param [String] name the name to provide to the target. This will also
     #   be the value that other defaults will be based on.
     # @param [String,Symbol] type the type of build target to create.
     #
     # @return [Target] the target created.
-    # 
+    #
     def create_target(name,type=:native)
-      
+
       target = @registry.add_object Target.send(type)
       @project.properties['targets'] << target.identifier
-      
+
       target.name = name
-      
+
       build_configuration_list = @registry.add_object(ConfigurationList.configration_list)
       target.build_configuration_list = build_configuration_list.identifier
-      
+
       target.project = self
-      
+
       yield target if block_given?
-      
+
       target.save!
     end
-    
+
     #
     # Remove a target from the Xcode project.
-    # 
+    #
     # @note this will remove the first target that matches the specified name.
-    # 
+    #
     # @note this will remove only the project entry at the moment and not the
-    #   the files that may be associated with the target. All build phases, 
+    #   the files that may be associated with the target. All build phases,
     #   build files, and configurations will automatically be cleaned up when
     #   Xcode is opened.
-    # 
+    #
     # @param [String] name the name of the target to remove from the Xcode
     #   project.
     #
     # @return [Target] the target that has been removed.
-    # 
+    #
     def remove_target(name)
       found_target = targets.find {|target| target.name == name }
       if found_target
@@ -327,9 +327,9 @@ module Xcode
       end
       found_target
     end
-    
-    # 
-    # Prints to STDOUT a description of this project's targets, configuration and schemes.  
+
+    #
+    # Prints to STDOUT a description of this project's targets, configuration and schemes.
     #
     def describe
       puts "Project #{name} contains"
@@ -345,25 +345,25 @@ module Xcode
         puts "    + Test action   => target:#{s.test.target.name}, config:#{s.test.name}" unless s.test.nil?
       end
     end
-    
+
     private
-  
+
     #
-    # Using the sytem tool plutil, the specified project file is parsed and 
-    # converted to JSON, which is then converted to a hash object. This content 
-    # contains all the data within the project file and is used to create the 
+    # Using the sytem tool plutil, the specified project file is parsed and
+    # converted to JSON, which is then converted to a hash object. This content
+    # contains all the data within the project file and is used to create the
     # Registry.
-    # 
+    #
     # @return [Registry] the representation of the project, this is a Hash with
     #   additional methods to provide easier functionality.
-    # 
+    #
     def parse_pbxproj
       registry = Xcode::PLUTILProjectParser.parse "#{@path}/project.pbxproj"
-      
+
       class << registry
         include Xcode::Registry
       end
-      
+
       registry
     end
 
--- lib/xcode/scheme.rb
+++ lib/xcode/scheme.rb
@@ -1,59 +1,70 @@
 require 'nokogiri'
 
 module Xcode
-  
+
   # Schemes are an XML file that describe build, test, launch and profile actions
   # For the purposes of Xcoder, we want to be able to build and test
   # The scheme's build action only describes a target, so we need to look at launch for the config
   class Scheme
-    attr_reader :path, :name, :launch, :test
-    def initialize(path)
-      @path = File.expand_path(path)
-      @root = File.expand_path "#{@path}/../../../../"
-      @name = File.basename(path).gsub(/\.xcscheme$/,'')
-      doc = Nokogiri::XML(open(@path))
-      
-      @launch = parse_action(doc, 'launch')
-      @test = parse_action(doc, 'test')
-    end
-    
-    # def project
-    #   launch.target.project
-    # end
-    
-    def builder
-      Xcode::Builder.new(self)
-    end
-    
+
     #
     # Parse all the scheme files that can be found in the given project or workspace. Schemes
     # can be defined as `shared` schemes and then `user` specific schemes. Parsing
     # the schemes will load the shared ones and then the current acting user's
     # schemes.
-    # 
+    #
     # @return [Array<Scheme>] the shared schemes and user specific schemes found
     #   within the projet/workspace at the path defined for schemes.
     #
     def self.find_in_path(path)
-      shared_schemes = Dir["#{path}/xcshareddata/xcschemes/*.xcscheme"]
-      user_specific_schemes = Dir["#{path}/xcuserdata/#{ENV['USER']}.xcuserdatad/xcschemes/*.xcscheme"]
-      
-      (shared_schemes + user_specific_schemes).map do |scheme|
-        Xcode::Scheme.new(scheme)
+      all_schemes_paths(path).map do |scheme_path|
+        Xcode::Scheme.new(root: path, path: scheme_path)
       end
     end
-    
-    private 
-    
+
+    def self.find_in_project(project)
+      find_in_path(project.path)
+    end
+
+    attr_reader :path, :name, :launch, :test
+
+    def initialize(params={})
+      @path = File.expand_path params[:path]
+      @root = File.expand_path(File.join(params[:root],'..'))
+      @name = File.basename(path).gsub(/\.xcscheme$/,'')
+      doc = Nokogiri::XML(open(path))
+
+      @launch = parse_action(doc, 'launch')
+      @test = parse_action(doc, 'test')
+    end
+
+    def builder
+      Xcode::Builder.new(self)
+    end
+
+    private
+
+    def self.all_schemes_paths(path)
+      shared_schemes_paths(path) + current_user_schemes_paths(path)
+    end
+
+    def self.shared_schemes_paths(root)
+      Dir["#{root}/xcshareddata/xcschemes/*.xcscheme"]
+    end
+
+    def self.current_user_schemes_paths(root)
+      Dir["#{root}/xcuserdata/#{ENV['USER']}.xcuserdatad/xcschemes/*.xcscheme"]
+    end
+
     def parse_action(doc, action_name)
       action = doc.xpath("//#{action_name.capitalize}Action").first
       buildableReference = action.xpath('BuildableProductRunnable/BuildableReference').first
       return nil if buildableReference.nil?
-      
+
       project_name  = buildableReference['ReferencedContainer'].gsub(/^container:/,'')
-      project       = Xcode.project "#{@root}/#{project_name}"    
+      project       = Xcode.project File.join(@root,project_name)
       target_name   = buildableReference['BlueprintName']
-      
+
       project.target(target_name).config(action['buildConfiguration'])
     end
 
