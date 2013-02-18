533434496624e5234bf6b72c6f19495f0e2f5b50
Aaron Quint <aaron@quirkey.com>
2013-02-18 13:27:26 -0500

    Fix build target setup for scheme builder

--- lib/xcode/configuration_list.rb
+++ lib/xcode/configuration_list.rb
@@ -1,9 +1,9 @@
 module Xcode
   module ConfigurationList
-    
-    # 
+
+    #
     # @example configuration list
-    # 
+    #
     #     7165D47D146B4EA100DE2F0E /* Build configuration list for PBXNativeTarget "TestProject" */ = {
     #       isa = XCConfigurationList;
     #       buildConfigurations = (
@@ -13,73 +13,73 @@ module Xcode
     #       defaultConfigurationIsVisible = 0;
     #       defaultConfigurationName = Release;
     #     };
-    def self.configration_list
+    def self.configuration_list
       { 'isa' => 'XCConfigurationList',
         'buildConfigurations' => [],
         'defaultConfigurationIsVisible' => '0',
         'defaultConfigurationName' => '' }
     end
-    
+
     #
     # @return [Hash] a hash of symbol names to configuration names.
-    # 
+    #
     def self.symbol_config_name_to_config_name
       { :debug => 'Debug', :release => 'Release' }
     end
-    
+
     #
     # Create a configuration for this ConfigurationList. This configuration needs
     # to have a name.
-    # 
+    #
     # @note unique names are currently not enforced but likely necessary for the
     #   the target to be build successfully.
-    # 
+    #
     # @param [Types] name Description
     #
     def create_config(name)
-      
+
       name = ConfigurationList.symbol_config_name_to_config_name[name] if ConfigurationList.symbol_config_name_to_config_name[name]
-      
-      # @todo a configuration has additional fields that are ususally set with 
+
+      # @todo a configuration has additional fields that are ususally set with
       #   some target information for the title.
-      
+
       new_config = @registry.add_object(Configuration.default_properties(name))
       @properties['buildConfigurations'] << new_config.identifier
-      
+
       yield new_config if block_given?
-      
+
       new_config.save!
     end
-    
+
     #
     # @return [BuildConfiguration] the build configuration that is set to default;
     #   nil if no configuration has been set as default.
-    # 
+    #
     def default_config
       build_configurations.find {|config| config.name == default_configuration_name }
     end
-    
+
     #
     # @return [String] the name of the default build configuration; nil if no
     #   configuration has been set as default.
-    # 
+    #
     def default_config_name
       default_configuration_name
     end
-    
+
     #
     # @todo allow the ability for a configuration to set itself as default and/or
     #   let a configuration be specified as a parameter here. Though we need
     #   to check to see that the configuration is part of the this configuration
     #   list.
-    # 
-    # @param [String] name of the build configuration to set as the default 
+    #
+    # @param [String] name of the build configuration to set as the default
     #   configuration; specify nil if you want to remove any default configuration.
     #
     def set_default_config(name)
       # @todo ensure that the name specified is one of the available configurations
       @properties['defaultConfigurationName'] = name
     end
-    
+
   end
 end
--- lib/xcode/scheme.rb
+++ lib/xcode/scheme.rb
@@ -5,7 +5,8 @@ module Xcode
   # Schemes are an XML file that describe build, test, launch and profile actions
   # For the purposes of Xcoder, we want to be able to build and test
   class Scheme
-    attr_reader :parent, :path, :name, :build_config, :build_targets
+    attr_reader :parent, :path, :name, :build_targets
+    attr_accessor :build_config
 
     #
     # Parse all the schemes given the current project.
@@ -19,21 +20,21 @@ module Xcode
     #
     def self.find_in_workspace(workspace)
       schemes = find_in_path(workspace, workspace.path)
-      
+
       # Project level schemes
       workspace.projects.each do |project|
         schemes+=project.schemes
       end
-      
+
       schemes
     end
-    
+
     # Parse all the scheme files that can be found at the given path. Schemes
     # can be defined as `shared` schemes and then `user` specific schemes. Parsing
     # the schemes will load the shared ones and then the current acting user's
     # schemes.
     #
-    # 
+    #
     # @param project or workspace in which the scheme is contained
     # @return [Array<Scheme>] the shared schemes and user specific schemes found
     #   within the project/workspace at the path defined for schemes.
@@ -43,17 +44,17 @@ module Xcode
         Xcode::Scheme.new(parent: parent, root: path, path: scheme_path)
       end
     end
-    
+
     def initialize(params={})
       @parent = params[:parent]
       @path = File.expand_path params[:path]
       @root = File.expand_path(File.join(params[:root],'..'))
       @name = File.basename(path).gsub(/\.xcscheme$/,'')
-      doc = Nokogiri::XML(open(@path))      
-      
+      doc = Nokogiri::XML(open(@path))
+
       parse_build_actions(doc)
     end
-    
+
     # Returns a builder for building this scheme
     def builder
       Xcode::Builder::SchemeBuilder.new(self)
@@ -84,23 +85,23 @@ module Xcode
     def self.current_user_schemes_paths(root)
       Dir["#{root}/xcuserdata/#{ENV['USER']}.xcuserdatad/xcschemes/*.xcscheme"]
     end
-    
+
     def target_from_build_reference(buildableReference)
       project_name  = buildableReference['ReferencedContainer'].gsub(/^container:/,'')
       target_name   = buildableReference['BlueprintName']
-      project_path  = File.join @root, project_name  
-      project       = Xcode.project project_path 
+      project_path  = File.join @root, project_name
+      project       = Xcode.project project_path
       project.target(target_name)
     end
-    
+
     def parse_build_actions(doc)
       # Build Config
       @build_targets = []
-      
+
       @build_config = doc.xpath("//LaunchAction").first['buildConfiguration']
-      
+
       build_action_entries = doc.xpath("//BuildAction//BuildableReference").each do |ref|
-        @build_targets << target_from_build_reference(ref) 
+        @build_targets << target_from_build_reference(ref)
       end
     end
 
