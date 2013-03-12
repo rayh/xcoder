1ab64aad7b3f6b06e21e6af88aafed97c0f18901
Ray Hilton <ray@wirestorm.net>
2013-03-12 15:14:37 +1100

    Add basic kickfolio support, fix s3 url generation

---
+++ lib/xcode/deploy/kickfolio.rb
@@ -0,0 +1,15 @@
+module Xcode
+	module Deploy
+		class Kickfolio
+			def initialize(options={})
+				@options = options
+			end
+
+			def deploy
+				RestClient.post "https://kickfolio.com/api/apps/#{options[:app_id]}", 
+					{:bundle_url => @options[:url], :auth_token: @options[:api_key]}
+					:content_type => 'application/json'
+			end
+		end
+	end
+end
