e0b4ba95fe1157a0e8e9f358cf7a958f1df221cf
Ray Hilton <ray@wirestorm.net>
2013-05-30 21:38:49 +1000

    Wait for process exit status before continuing

--- lib/xcode/shell.rb
+++ lib/xcode/shell.rb
@@ -15,14 +15,14 @@ module Xcode
     def self.execute(cmd, show_output=true, show_command=false)
       out = []
       cmd = cmd.to_s
-      
-      PTY.spawn(cmd) do |r, w, pid|
+      PTY.spawn(cmd) do |r, w, child_pid|
         r.sync
         r.each_line do |line|
           puts line if show_output
           yield(line) if block_given?
           out << line
         end 
+        Process.wait(pid)
       end
       raise ExecutionError.new("Error (#{$?.exitstatus}) executing '#{cmd}'", out) if $?.exitstatus>0
       out
